908705a576aa5b5709ef78e981201ae06fb43c15
MATC Student <student@MATC1234.(none)>
2014-04-07 20:17:02 -0500

    sessions

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,6 +6,9 @@ describe User do
 
   subject { @user }
 
+  it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
+  #it { should respond_to(:authenticate) }
   it { should respond_to(:name) }
   it { should respond_to(:email) }
   it { should respond_to(:password_digest) }
