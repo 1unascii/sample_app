7c6048d1c586c13b033bb6ee391bcadd7f3ec04d
MATC Student <student@MATC1234.(none)>
2014-04-14 16:54:46 -0500

    more signing in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,15 +6,13 @@ describe User do
 
   subject { @user }
 
-  it { should respond_to(:password_confirmation) }
-  it { should respond_to(:remember_token) }
-  #it { should respond_to(:authenticate) }
   it { should respond_to(:name) }
   it { should respond_to(:email) }
   it { should respond_to(:password_digest) }
   it { should respond_to(:authenticate) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
   it { should be_valid }
 
   describe "when name is not present" do
@@ -91,5 +89,8 @@ describe User do
       expect(@user.reload.email).to eq mixed_case_email.downcase
     end
   end
-  
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
 end
